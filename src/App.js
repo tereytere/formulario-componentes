94662c1224ec1a17a8508d1a4d5d66bc586a14fc
tereytere <116809352+tereytere@users.noreply.github.com>
2024-08-21 17:33:02 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import Table from './components/Table';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Formulario from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Table />
       <Formulario />
-      <Footer />
     </div>
   );
 }
